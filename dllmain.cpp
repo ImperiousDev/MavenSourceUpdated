579fe4e679355afdc226e57077e2d389266e0508
illegal-instuction-co <79029454+illegal-instuction-co@users.noreply.github.com>
2021-03-02 02:03:57 +0300

    Readable code
    Readable code

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,31 +2,24 @@
 
 
 VOID Main() {
+    
     Util::CreateConsole();
+    
     MH_Initialize();
+    
     SettingsHelper::Initialize();
 
-    if (!Util::Initialize()) {
-        return;
-    }
+    if (!Util::Initialize()) return;
 
-    if (!Offsets::Initialize()) {
-        return;
-    }
+    if (!Offsets::Initialize()) return;
 
-    if (!Core::Initialize()) {
-        return;
-    }
+    if (!Core::Initialize()) return;
 
-    if (!Render::Initialize()) {
-        return;
-    }
+    if (!Render::Initialize()) return;
 }
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved) {
-    if (reason == DLL_PROCESS_ATTACH) {
-        Main();
-    }
+    if (reason == DLL_PROCESS_ATTACH) Main();
 
     return TRUE;
 }
